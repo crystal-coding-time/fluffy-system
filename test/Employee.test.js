7ea5dcbf4f346831683bd08bc7c02a91b9624a54
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-01-24 17:39:33 -0500

    Added tests to employee.test.js

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -9,3 +9,31 @@ test('Creates an employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+// Gets name from getName() method
+test('Gets employee name', () => {
+    const employee = new Employee('Eli', 30, 'test@test.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// Gets ID from getID() method
+test('Gets employee ID', () => {
+    const employee = new Employee('Eli', 30, 'test@test.com');
+
+    expect(employee.getID()).toEqual(expect.any(Number));
+});
+
+// Gets email from getEmail() method
+test('Gets employee email', () => {
+    const employee = new Employee('Eli', 30, 'test@test.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+// Gets role from getRole() method
+test('Gets role of employee', () => {
+    const employee = new Employee('Eli', 30, 'test@test.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+}); 
