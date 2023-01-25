b6dac79e9aab6f6eef022cd5f6271afe02655914
The Vault <114878799+crystal-coding-time@users.noreply.github.com>
2023-01-25 14:23:22 -0500

    Pushing up additions

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Intern = require('./lib/Intern');
 
 // Inports node modules
 const fs = require('fs');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 const inquirer = ('inquirer');
 
 // Empty team array
@@ -57,11 +58,69 @@ const addManager = () => {
                 }
             }
         },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter manager's office number.",
+            validate: nameInput => {
+                if(isNaN(nameInput)) {
+                    console.log ('Please enter an office number.')
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    ])
+.then(managerInput => {
+    const  { name, id, email, officeNumber } = managerInput; 
+    const manager = new Manager (name, id, email, officeNumber);
 
+    teamArray.push(manager);
+    console.log(manager);
+})
+};
 
+const addEmployee = () => {
+    console.log(`
+    ---------------
+    Adding team members
+    ---------------`);
 
+    return inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'role',
+        message: 'Choose employee role.',
+        Choices: ['Engineer', 'Intern']
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the employee's name?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log("Please enter a name.");
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "Please enter the emplyee's ID.",
+        validate: nameInput => {
+            if  (isNaN(nameInput)) {
+                console.log ("Please enter the employee's ID!")
+                return false; 
+            } else {
+                return true;
+            }
+        }
+    },
 
-
-        
+    
     ])
 }
